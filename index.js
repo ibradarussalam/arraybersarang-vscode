5ebdf895662df6e18ead59c7b5dd3672d408e9ca
ibradarussalam <aibiareigaming@gmail.com>
2023-12-08 04:41:05 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+let daerah = ['indonesia','malaysia','singapura',['Sumatra Barat','Sumatra Selatan','Sumatra Utara',['Padang','Solok','Pariaman','Bukitinggi',['Padang Utara','Nanggalo','Lubeg',['Alai Parak Kopi','Lolong Belanti']]]]]
+
+
+console.log(`Nama saya ibra Darussalam yang beralamat di negara ${daerah[0]}, dan bertempat di provinsi ${daerah[3][0]}, dan kota ${daerah[3][3][0]}, yaitu kecamatan ${daerah[3][3][4][0]}, yang bertepatan dijalan ${daerah[3][3][4][3][0]}.`)
